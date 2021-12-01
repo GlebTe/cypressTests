d97139b6cf72c30ada52cb5ea455a812ea81373c
Gleb Tebaykin <reider092@gmail.com>
2021-12-02 03:33:24 +0500

    wrongCredAuth
    test done

--- cypress/integration/wrongCredAuthTest/wrongCredAuthTest.js
+++ cypress/integration/wrongCredAuthTest/wrongCredAuthTest.js
@@ -6,7 +6,10 @@ it('authorisation test', function(){
     cy.get('.value > :nth-child(1) > div').click()
     cy.contains('Вход под СМС') .click() 
     cy.get('.text').type('demobank')
-    cy.get('.password').type('123456')
+    const uuid = () => Cypress._.random(0, 1e6)
+    const id = uuid()
+    const testname = `testname${id}`
+    cy.get('.password').type(testname)
     cy.get('#login').click()
     cy.get('.integer').type('123456')
     cy.get('#login').click()
