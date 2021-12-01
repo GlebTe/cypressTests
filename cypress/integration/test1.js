2e43c457735709b02ac6c2c660543d18d864a70a
Gleb Tebaykin <reider092@gmail.com>
2021-12-01 20:47:04 +0500

    first attempt

---
+++ cypress/integration/test1.js
@@ -0,0 +1,13 @@
+/// <reference types ="cypress" />
+
+
+it('google test', function(){
+    cy.visit('https://isfront.ru/login')
+    cy.get('.value > :nth-child(1) > div').click()
+    cy.contains('Вход под СМС') .click() 
+    cy.get('.text').type('demobank')
+    cy.get('.password').type('123456')
+    cy.get('#login').click()
+    cy.get('.integer').type('123456')
+    cy.get('#login').click()
+})
